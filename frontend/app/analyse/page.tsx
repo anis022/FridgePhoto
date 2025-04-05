d49d118260c65344c0f3c88abaca27e1897775ec
GeorgesAbiChahine <georgesabichahine1@gmail.com>
2025-04-04 22:28:19 -0400

    advanced in analysis page

--- frontend/app/analyse/page.tsx
+++ frontend/app/analyse/page.tsx
@@ -1,13 +1,63 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Camera, Folder } from "lucide-react";
+import { AnimatedGroup } from "@/components/ui/animated-group";
+const transitionVariants = {
+  item: {
+    hidden: {
+      opacity: 0,
+      filter: "blur(12px)",
+      y: 12,
+    },
+    visible: {
+      opacity: 1,
+      filter: "blur(0px)",
+      y: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.3,
+        duration: 1.5,
+      },
+    },
+  },
+};
 
 function Analyse() {
   return (
     <>
-        <div className="flex h-screen w-full items-center justify-center bg-background p-4 text-foreground">
-            <h1 className="text-2xl font-bold">Analyse</h1>
-        </div>
+      <AnimatedGroup variants={transitionVariants}>
+        <section className="py-16 md:py-32">
+          <div className="mx-auto max-w-5xl px-6">
+            <div className="text-center">
+              <h2 className="text-balance text-4xl font-semibold lg:text-5xl">
+                It's time to get cookin'
+              </h2>
+              <p className="mt-4">
+                Please provide us with a photo of your fridge so we can start
+                our analysis.
+              </p>
+
+              <div className="mt-12 flex flex-wrap justify-center gap-4">
+                <Button asChild size="lg">
+                  <Link href="/">
+                    <Camera className="size-6" aria-hidden />
+                    <span>Take a photo</span>
+                  </Link>
+                </Button>
+
+                <Button asChild size="lg" variant="outline">
+                  <Link href="/">
+                    <Folder className="size-6" aria-hidden />
+                    <span>Upload a photo</span>
+                  </Link>
+                </Button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </AnimatedGroup>
     </>
-  )
+  );
 }
 
-export default Analyse
+export default Analyse;
