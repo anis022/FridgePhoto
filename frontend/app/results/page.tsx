788657a90cce0ef2774b3661a785c1575e2f024f
GeorgesAbiChahine <georgesabichahine1@gmail.com>
2025-04-05 04:29:16 -0400

    Add environment variables, update dependencies, and implement results page

---
+++ frontend/app/results/page.tsx
@@ -0,0 +1,13 @@
+import HeroSection from '@/components/hero-section'
+import { HeroHeader } from '@/components/hero5-header'
+import React from 'react'
+
+function Results() {
+  return (
+    <>
+    <HeroSection></HeroSection>
+    </>
+  )
+}
+
+export default Results
